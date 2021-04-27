606036bd001e6c933be9a5eb007ac673dd318808
drasko-kosovic <drasko.kosovic@montefarm.co.me>
2021-04-27 08:21:44 +0200

    mail

--- src/main/java/tenderi/repository/HvalePonudeRepository.java
+++ src/main/java/tenderi/repository/HvalePonudeRepository.java
@@ -1,6 +1,8 @@
 package tenderi.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tenderi.domain.HvalePonude;
 
--- src/main/java/tenderi/repository/SpecifikacijaRepository.java
+++ src/main/java/tenderi/repository/SpecifikacijaRepository.java
@@ -1,8 +1,11 @@
 package tenderi.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tenderi.domain.Specifikacija;
+import tenderi.domain.ViewVrednovanje;
 
 /**
  * Spring Data SQL repository for the Specifikacija entity.
